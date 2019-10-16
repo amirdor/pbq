d82aeaad03e9318fafbf0d1524b9882cbf4f4b19
doramir <amirdor@gmail.com>
2019-10-16 16:37:53 +0300

    Update test_query.py

--- tests/test_query.py
+++ tests/test_query.py
@@ -18,7 +18,7 @@ class TestQuery(unittest.TestCase):
     @mock.patch('google.cloud.bigquery.Client')
     def test_query_format(self, mock_Client):
         cwd = os.getcwd()
-        query_file = '{}/files/sample_query.sql'.format(cwd)
+        query_file = '{}/tests/files/sample_query.sql'.format(cwd)
         query = Query.read_file(query_file)
         q_file = open(query_file, 'r')
         _query = q_file.read()
@@ -27,18 +27,18 @@ class TestQuery(unittest.TestCase):
     @mock.patch('google.cloud.bigquery.Client')
     def test_query_format_with_variables(self, mock_Client):
         cwd = os.getcwd()
-        with open('{}/files/init.json'.format(cwd)) as json_file:
+        with open('{}/tests/files/init.json'.format(cwd)) as json_file:
             data = json.load(json_file)
-        query = Query.read_file('files/variable_query.sql', parameters=data)
-        q_file = open('files/sample_query.sql', 'r')
+        query = Query.read_file('{}/tests/files/variable_query.sql'.format(cwd), parameters=data)
+        q_file = open('{}/tests/files/sample_query.sql'.format(cwd), 'r')
         _query = q_file.read()
         self.assertEqual(query.query, _query)
 
     @mock.patch('google.cloud.bigquery.Client')
     def test_query_format_with_variables_as_dict(self, mock_Client):
         cwd = os.getcwd()
-        query = Query.read_file('{}/files/variable_query.sql'.format(cwd), parameters={'until_day': 1})
-        q_file = open('{}/files/sample_query.sql'.format(cwd), 'r')
+        query = Query.read_file('{}/tests/files/variable_query.sql'.format(cwd), parameters={'until_day': 1})
+        q_file = open('{}/tests/files/sample_query.sql'.format(cwd), 'r')
         _query = q_file.read()
         self.assertEqual(query.query, _query)
 
@@ -46,4 +46,4 @@ class TestQuery(unittest.TestCase):
     def test_query_format_with_missing_values(self, mock_Client):
         cwd = os.getcwd()
         with self.assertRaises(ValueError):
-            Query.read_file('{}/files/variable_query.sql'.format(cwd))
+            Query.read_file('{}/tests/files/variable_query.sql'.format(cwd))
